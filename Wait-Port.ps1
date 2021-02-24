81813984c72edd56a560b2a0afbedf505f3e8f4c
Brecht Laitem <brecht.laitem@cogenius.be>
2021-02-24 11:44:04 +0100

    Success and failure command

--- Wait-Port.ps1
+++ Wait-Port.ps1
@@ -13,7 +13,15 @@ param (
             
     [Parameter(Position = 2)]
     [ValidateNotNullOrEmpty()]
-    [int]$Timeout = 60
+    [int]$Timeout = 60,
+
+    [Parameter(Position = 3)]
+    [ValidateNotNullOrEmpty()]
+    [string]$SuccessCommand,
+
+    [Parameter(Position = 4)]
+    [ValidateNotNullOrEmpty()]
+    [string]$FailureCommand
 )
 $callerEA = $ErrorActionPreference
 $ErrorActionPreference = 'Stop'
@@ -25,15 +33,24 @@ Write-Host "Test ${port} at host ${ComputerName}"
 try {
     do {
         if ($sw.elapsed -gt $to) {
+            if (-not ([string]::IsNullOrEmpty($FailureCommand))) {
+                Write-Verbose "Execute the failure command"
+                $failureScriptBlock = [scriptblock]::Create($FailureCommand)
+                Invoke-Command -scriptBlock $failureScriptBlock
+            }
             throw "Can't connect to port ${port} at host ${ComputerName}"
         }
         Write-Verbose "Connecting to port ${port} at host ${ComputerName}..."
-        sleep 1
-    } until(Test-NetConnection $ComputerName -Port $Port | ? { $_.TcpTestSucceeded })
-    Write-Verbose "Connected successfully to port ${port} at host ${ComputerName}"            
+        Start-Sleep 1
+    } until(Test-NetConnection $ComputerName -Port $Port | Where-Object { $_.TcpTestSucceeded })
+    Write-Verbose "Connected successfully to port ${port} at host ${ComputerName}"   
+    
+    if (-not ([string]::IsNullOrEmpty($SuccessCommand))) {
+        Write-Verbose "Execute the success command"
+        $successScriptBlock = [scriptblock]::Create($SuccessCommand)
+        Invoke-Command -scriptBlock $successScriptBlock
+    }
 }
 catch {
     Write-Error -ErrorRecord $_ -EA $callerEA
-}    
-    
-
+}
